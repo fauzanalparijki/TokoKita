9fc2fa41c54db7c526288ad7aa2ad2ba5a75f277
fauzanalparijki <fauzanalparijki4@gmail.com>
2026-08-21 11:17:47 +0700

    Rename script (1).js to script.js

--- script (1).js
+++
@@ -1,1 +0,0 @@
-let cart=0;function addCart(){cart++;document.getElementById("cartCount").textContent=cart}document.getElementById("year").textContent=new Date().getFullYear();
---
+++ script.js
@@ -0,0 +1,1 @@
+let cart=0;function addCart(){cart++;document.getElementById("cartCount").textContent=cart}document.getElementById("year").textContent=new Date().getFullYear();
